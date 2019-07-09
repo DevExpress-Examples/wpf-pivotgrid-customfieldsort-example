b03a2e26ad784a3cbae160147cd8865b301d6567
BrianDX <boris.zaitsev@devexpress.com>
2019-07-09 21:02:19 +0300

    example content

---
+++ CS/Wpf-PivotGrid-CustomFieldSort-Example/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using DevExpress.Xpf.Editors;
+using DevExpress.Xpf.PivotGrid;
+using System;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+
+namespace Wpf_PivotGrid_CustomFieldSort_Example
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<MyOrderRow> OrderSourceList {get; set;}
+        public MainWindow()
+        {
+            InitializeComponent();
+            pivotGridControl1.CustomFieldSort += PivotGridControl1_CustomFieldSort;
+            checkEdit1.Checked += CheckEdit1_Checked;
+            checkEdit1.Unchecked += CheckEdit1_Unchecked;
+        }
+
+        private void CheckEdit1_Unchecked(object sender, RoutedEventArgs e)
+        {
+            // Set the default sorting algorithm.
+            fieldSalesPerson.SortMode = FieldSortMode.Default;
+        }
+
+        private void CheckEdit1_Checked(object sender, RoutedEventArgs e)
+        {
+            // Enable the CustomFieldSort event for the fieldSalesPerson field.
+            fieldSalesPerson.SortMode = FieldSortMode.Custom;
+        }
+
+        private void PivotGridControl1_CustomFieldSort(object sender, DevExpress.Xpf.PivotGrid.PivotCustomFieldSortEventArgs e)
+        {
+            if (e.Field.FieldName == "SalesPersonName")
+            {
+                int result;
+                if (e.SortLocation == FieldSortLocation.Pivot)
+                {
+                    object orderValue1 = e.GetListSourceColumnValue(e.ListSourceRowIndex1, "SalesPersonId"),
+                        orderValue2 = e.GetListSourceColumnValue(e.ListSourceRowIndex2, "SalesPersonId");
+                    result = Comparer.Default.Compare(orderValue1, orderValue2);
+                }
+                else
+                {
+                    // Compare last names.
+                    result = Comparer.Default.Compare(e.Value1.ToString().Split(' ')[1], e.Value2.ToString().Split(' ')[1]);
+                    // If last names are the same, compare first names.
+                    if (result == 0)
+                        result = Comparer.Default.Compare(e.Value1.ToString().Split(' ')[0], e.Value2.ToString().Split(' ')[0]);
+                }
+                e.Result = result;
+                e.Handled = true;
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            OrderSourceList = DatabaseHelper.CreateData();
+            pivotGridControl1.DataSource = OrderSourceList;
+            pivotGridControl1.BestFitArea = DevExpress.Xpf.PivotGrid.FieldBestFitArea.FieldHeader;
+            pivotGridControl1.BestFit();
+        }
+    }
+}
---
+++ CS/Wpf-PivotGrid-CustomFieldSort-Example/MyOrder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_PivotGrid_CustomFieldSort_Example
+{
+    public class MyOrderRow
+    {
+        public int ID { get; internal set; }
+        public DateTime OrderDate { get; internal set; }
+        public int Quantity { get; internal set; }
+        public decimal UnitPrice { get; internal set; }
+        public string CustomerName { get; internal set; }
+        public string ProductName { get; internal set; }
+        public string CategoryName { get; internal set; }
+        public string SalesPersonName { get; internal set; }
+        public int SalesPersonId { get; internal set; }
+        public decimal ExtendedPrice { get; internal set; }
+    }
+}
