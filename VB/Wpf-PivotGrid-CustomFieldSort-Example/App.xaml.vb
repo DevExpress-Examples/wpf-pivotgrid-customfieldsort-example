57106ccf0a52ccf786e72b36b40ef944176c8257
DevExpressExampleBot <fakeEmail>
2022-12-18 23:21:23 +0400

    Source auto update [skip ci]

---
+++ VB/Wpf-PivotGrid-CustomFieldSort-Example/App.xaml.vb
@@ -0,0 +1,12 @@
+Imports System.Windows
+
+Namespace Wpf_PivotGrid_CustomFieldSort_Example
+
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Public Partial Class App
+        Inherits Application
+
+    End Class
+End Namespace
--- VB/Wpf-PivotGrid-CustomFieldSort-Example/DatabaseHelper.vb
+++ VB/Wpf-PivotGrid-CustomFieldSort-Example/DatabaseHelper.vb
@@ -1,108 +1,122 @@
-﻿Imports System
+Imports System
 Imports System.Collections.Generic
 Imports System.Collections.ObjectModel
 Imports System.Linq
 
 Namespace Wpf_PivotGrid_CustomFieldSort_Example
-	Public NotInheritable Class DatabaseHelper
 
-		Private Sub New()
-		End Sub
-		#Region "Fields"
+    Public Module DatabaseHelper
 
-		Private Shared ReadOnly random As New Random()
-		Private Shared ReadOnly FirstNames() As String = { "Julia", "Stephanie", "Alex", "John", "Curtis", "Keith", "Timothy", "Jack", "Miranda", "Alice" }
-		Private Shared ReadOnly LastNames() As String = { "Black", "White", "Brown", "Smith", "Cooper", "Parker", "Walker", "Hunter", "Burton", "Douglas", "Fox", "Simpson" }
-		Private Shared ReadOnly Adjectives() As String = { "Ancient", "Modern", "Mysterious", "Elegant", "Red", "Green", "Blue", "Amazing", "Wonderful", "Astonishing", "Lovely", "Beautiful", "Inexpensive", "Famous", "Magnificent", "Fancy" }
-		Private Shared ReadOnly ProductNames() As String = { "Ice Cubes", "Bicycle", "Desk", "Hamburger", "Notebook", "Tea", "Cellphone", "Butter", "Frying Pan", "Napkin", "Armchair", "Chocolate", "Yoghurt", "Statuette", "Keychain" }
-		Private Shared ReadOnly CategoryNames() As String = { "Business", "Presents", "Accessories", "Home", "Hobby" }
+#Region "Fields"
+        Private ReadOnly random As System.Random = New System.Random()
 
-		Private Shared ReadOnly CustomerNames() As String
-		Private Shared ReadOnly SalesPersonNames() As String
-		Private Shared ReadOnly Products() As ProductDataRecord
+        Private ReadOnly FirstNames As String() = {"Julia", "Stephanie", "Alex", "John", "Curtis", "Keith", "Timothy", "Jack", "Miranda", "Alice"}
 
-		#End Region
+        Private ReadOnly LastNames As String() = {"Black", "White", "Brown", "Smith", "Cooper", "Parker", "Walker", "Hunter", "Burton", "Douglas", "Fox", "Simpson"}
 
-		Shared Sub New()
-			CustomerNames = GenerateUniqueValues(random.Next(40, 50), AddressOf GeneratePersonName).ToArray()
-			SalesPersonNames = GenerateUniqueValues(8, AddressOf GeneratePersonName).ToArray()
-			Products = GenerateProducts()
-		End Sub
+        Private ReadOnly Adjectives As String() = {"Ancient", "Modern", "Mysterious", "Elegant", "Red", "Green", "Blue", "Amazing", "Wonderful", "Astonishing", "Lovely", "Beautiful", "Inexpensive", "Famous", "Magnificent", "Fancy"}
 
-		#Region "Public"
-		Public Shared Function CreateData() As ObservableCollection(Of MyOrderRow)
-			Dim orderList As New ObservableCollection(Of MyOrderRow)()
-			For i As Integer = 0 To 1499
-				Dim row As New MyOrderRow()
-				row.ID = i
-				Dim product = DatabaseHelper.GetProduct()
-				row.OrderDate = DatabaseHelper.GetOrderDate()
-				row.Quantity = DatabaseHelper.GetQuantity()
-				row.UnitPrice = DatabaseHelper.GetProductPrice(product)
-				row.ExtendedPrice = row.Quantity * row.UnitPrice
-				row.CustomerName = DatabaseHelper.GetCustomerName()
-				row.ProductName = product.ProductName
-				row.CategoryName = product.CategoryName
-				row.SalesPersonName = DatabaseHelper.GetSalesPersonName()
-				row.SalesPersonId = Array.IndexOf(SalesPersonNames, row.SalesPersonName)
-				row.SalesPersonName = row.SalesPersonName & " " & row.SalesPersonId
-				orderList.Add(row)
-			Next i
-			Return orderList
-		End Function
-		Public Shared Function GetOrderDate() As Date
-			Return New Date(random.Next(2017, 2019), random.Next(1, 13), random.Next(1, 28))
-		End Function
-		Public Shared Function GetQuantity() As Integer
-			Return random.Next(1, 100)
-		End Function
-		Public Shared Function GetProductPrice(ByVal product As ProductDataRecord) As Decimal
-			Dim price = product.UnitPrice * CDec(0.5 + random.NextDouble())
-			Return Math.Round(price, 2)
-		End Function
-		Public Shared Function GetProduct() As ProductDataRecord
-			Return Products(random.Next(Products.Length))
-		End Function
-		Public Shared Function GetCustomerName() As String
-			Return CustomerNames(random.Next(CustomerNames.Length))
-		End Function
-		Public Shared Function GetSalesPersonName() As String
-			Return SalesPersonNames(random.Next(SalesPersonNames.Length))
-		End Function
+        Private ReadOnly ProductNames As String() = {"Ice Cubes", "Bicycle", "Desk", "Hamburger", "Notebook", "Tea", "Cellphone", "Butter", "Frying Pan", "Napkin", "Armchair", "Chocolate", "Yoghurt", "Statuette", "Keychain"}
 
-		#End Region
-		#Region "Private"
+        Private ReadOnly CategoryNames As String() = {"Business", "Presents", "Accessories", "Home", "Hobby"}
 
-		Private Shared Function GenerateUniqueValues(Of T)(ByVal count As Integer, ByVal generateValue As Func(Of T)) As List(Of T)
-			Dim values = New HashSet(Of T)()
-			Do While values.Count < count
-				Dim value = generateValue()
-				If Not values.Contains(value) Then
-					values.Add(value)
-				End If
-			Loop
-			Return values.ToList()
-		End Function
+        Private ReadOnly CustomerNames As String()
 
-		Private Shared Function GenerateProducts() As ProductDataRecord()
-			Return GenerateUniqueValues(random.Next(80, 100), AddressOf GenerateProductName).Select(Function(productName) New ProductDataRecord With {.ProductName = productName, .UnitPrice = random.Next(10, 500), .CategoryName = GenerateCategoryName()}).ToArray()
-		End Function
-		Private Shared Function GenerateCategoryName() As String
-			Return CategoryNames(random.Next(CategoryNames.Length))
-		End Function
-		Private Shared Function GeneratePersonName() As String
-			Return FirstNames(random.Next(FirstNames.Length)) & " " & LastNames(random.Next(LastNames.Length))
-		End Function
-		Private Shared Function GenerateProductName() As String
-			Return Adjectives(random.Next(Adjectives.Length)) & " " & ProductNames(random.Next(ProductNames.Length))
-		End Function
-		#End Region
-	End Class
+        Private ReadOnly SalesPersonNames As String()
 
-	Public Class ProductDataRecord
-		Public Property ProductName() As String
-		Public Property CategoryName() As String
-		Public Property UnitPrice() As Decimal
-	End Class
+        Private ReadOnly Products As Wpf_PivotGrid_CustomFieldSort_Example.ProductDataRecord()
 
+#End Region
+        Sub New()
+            Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.CustomerNames = Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.GenerateUniqueValues(Of System.[String])(CInt((Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.random.[Next](CInt((40)), CInt((50))))), CType((AddressOf Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.GeneratePersonName), System.Func(Of System.[String]))).ToArray()
+            Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.SalesPersonNames = Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.GenerateUniqueValues(Of System.[String])(CInt((8)), CType((AddressOf Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.GeneratePersonName), System.Func(Of System.[String]))).ToArray()
+            Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.Products = Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.GenerateProducts()
+        End Sub
+
+#Region "Public"
+        Public Function CreateData() As ObservableCollection(Of Wpf_PivotGrid_CustomFieldSort_Example.MyOrderRow)
+            Dim orderList As System.Collections.ObjectModel.ObservableCollection(Of Wpf_PivotGrid_CustomFieldSort_Example.MyOrderRow) = New System.Collections.ObjectModel.ObservableCollection(Of Wpf_PivotGrid_CustomFieldSort_Example.MyOrderRow)()
+            For i As Integer = 0 To 1500 - 1
+                Dim row As Wpf_PivotGrid_CustomFieldSort_Example.MyOrderRow = New Wpf_PivotGrid_CustomFieldSort_Example.MyOrderRow()
+                row.ID = i
+                Dim product = Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.GetProduct()
+                row.OrderDate = Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.GetOrderDate()
+                row.Quantity = Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.GetQuantity()
+                row.UnitPrice = Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.GetProductPrice(product)
+                row.ExtendedPrice = row.Quantity * row.UnitPrice
+                row.CustomerName = Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.GetCustomerName()
+                row.ProductName = product.ProductName
+                row.CategoryName = product.CategoryName
+                row.SalesPersonName = Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.GetSalesPersonName()
+                row.SalesPersonId = System.Array.IndexOf(Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.SalesPersonNames, row.SalesPersonName)
+                row.SalesPersonName = row.SalesPersonName & " " & row.SalesPersonId
+                orderList.Add(row)
+            Next
+
+            Return orderList
+        End Function
+
+        Public Function GetOrderDate() As DateTime
+            Return New System.DateTime(Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.random.[Next](2017, 2019), Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.random.[Next](1, 13), Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.random.[Next](1, 28))
+        End Function
+
+        Public Function GetQuantity() As Integer
+            Return Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.random.[Next](1, 100)
+        End Function
+
+        Public Function GetProductPrice(ByVal product As Wpf_PivotGrid_CustomFieldSort_Example.ProductDataRecord) As Decimal
+            Dim price = product.UnitPrice * CDec((0.5 + Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.random.NextDouble()))
+            Return System.Math.Round(price, 2)
+        End Function
+
+        Public Function GetProduct() As ProductDataRecord
+            Return Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.Products(Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.random.[Next](Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.Products.Length))
+        End Function
+
+        Public Function GetCustomerName() As String
+            Return Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.CustomerNames(Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.random.[Next](Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.CustomerNames.Length))
+        End Function
+
+        Public Function GetSalesPersonName() As String
+            Return Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.SalesPersonNames(Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.random.[Next](Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.SalesPersonNames.Length))
+        End Function
+
+#End Region
+#Region "Private"
+        Private Function GenerateUniqueValues(Of T)(ByVal count As Integer, ByVal generateValue As System.Func(Of T)) As List(Of T)
+            Dim values = New System.Collections.Generic.HashSet(Of T)()
+            While values.Count < count
+                Dim value = generateValue()
+                If Not values.Contains(value) Then values.Add(value)
+            End While
+
+            Return values.ToList()
+        End Function
+
+        Private Function GenerateProducts() As Wpf_PivotGrid_CustomFieldSort_Example.ProductDataRecord()
+            Return GenerateUniqueValues(Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.random.[Next](80, 100), New Global.System.Func(Of System.String)(AddressOf Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.GenerateProductName)).[Select](Function(productName) New Wpf_PivotGrid_CustomFieldSort_Example.ProductDataRecord With {.ProductName = productName, .UnitPrice = Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.random.[Next](10, 500), .CategoryName = Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.GenerateCategoryName()}).ToArray()
+        End Function
+
+        Private Function GenerateCategoryName() As String
+            Return Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.CategoryNames(Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.random.[Next](Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.CategoryNames.Length))
+        End Function
+
+        Private Function GeneratePersonName() As String
+            Return Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.FirstNames(Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.random.[Next](Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.FirstNames.Length)) & " " & Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.LastNames(Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.random.[Next](Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.LastNames.Length))
+        End Function
+
+        Private Function GenerateProductName() As String
+            Return Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.Adjectives(Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.random.[Next](Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.Adjectives.Length)) & " " & Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.ProductNames(Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.random.[Next](Wpf_PivotGrid_CustomFieldSort_Example.DatabaseHelper.ProductNames.Length))
+        End Function
+#End Region
+    End Module
+
+    Public Class ProductDataRecord
+
+        Public Property ProductName As String
+
+        Public Property CategoryName As String
+
+        Public Property UnitPrice As Decimal
+    End Class
 End Namespace
